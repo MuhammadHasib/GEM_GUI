94a2082e5043024d9afa75ce26484602b2378678
ola <ola@192.168.1.2>
2016-07-30 02:13:17 +0300

    Editing tracking panel design and assets

--- track_parts.php
+++ track_parts.php
@@ -2890,9 +2890,7 @@ include "side.php";
             </div>
         </div>-->
 
-        <a href="#" id="btn-scroll-up" class="btn-scroll-up btn btn-sm btn-inverse">
-            <i class="ace-icon fa fa-angle-double-up icon-only bigger-110"></i>
-        </a>
+        
     </div><!-- /.main-container -->
 
 <?php
