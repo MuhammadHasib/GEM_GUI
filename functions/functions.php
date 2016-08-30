a87bbcd3f45408589731982151ec247e2b4ddd56
ola <ola@olamac.dyndns.cern.ch>
2016-08-30 17:04:46 +0300

    - edit query to get GEB with no childs

--- functions/functions.php
+++ functions/functions.php
@@ -375,8 +375,8 @@ function get_available_parts_nochild($part_id, $version) {
     $conn = database_connection();
     global $GEB_KIND_OF_PART_ID;
     if($part_id == $GEB_KIND_OF_PART_ID ){
-       $sql = "SELECT SERIAL_NUMBER FROM CMS_GEM_CORE_CONSTRUCT.PARTS WHERE KIND_OF_PART_ID='" . $part_id . "' AND SERIAL_NUMBER LIKE '%" . $version . "%' AND (select count(PART_PARENT_ID) from CMS_GEM_CORE_CONSTRUCT.PHYSICAL_PARTS_TREE) < 25 ORDER BY SUBSTR(SERIAL_NUMBER, -4)  asc"; 
-       echo $sql;
+       $sql = "SELECT SERIAL_NUMBER FROM CMS_GEM_CORE_CONSTRUCT.PARTS WHERE KIND_OF_PART_ID='" . $part_id . "' AND SERIAL_NUMBER LIKE '%" . $version . "%' AND (select COUNT(PART_PARENT_ID) from CMS_GEM_CORE_CONSTRUCT.PHYSICAL_PARTS_TREE) < 25 ORDER BY SUBSTR(SERIAL_NUMBER, -4)  asc"; 
+       //echo $sql;
     }
     else{
     $sql = "SELECT SERIAL_NUMBER FROM CMS_GEM_CORE_CONSTRUCT.PARTS WHERE KIND_OF_PART_ID='" . $part_id . "' AND SERIAL_NUMBER LIKE '%" . $version . "%' AND PART_ID not in (select PART_PARENT_ID from CMS_GEM_CORE_CONSTRUCT.PHYSICAL_PARTS_TREE) ORDER BY SUBSTR(SERIAL_NUMBER, -4)  asc"; //select data or insert data 
