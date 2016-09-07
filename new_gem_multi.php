79664da3f76c293fdae9293b4c8829a672723a86
ola <ola@olamac.dyndns.cern.ch>
2016-09-07 15:45:51 +0300

    New Feature: add FOIL/s history

--- new_gem_multi.php
+++ new_gem_multi.php
@@ -11,8 +11,18 @@ include "head.php";
 
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
             <h1 class="page-header">GEM FOIL History </h1>
+             <div class="col-xs-6 panel-info panel" style="padding-left: 0px; padding-right: 0px;">
+                            <div class="panel-heading">
+                                <h3 class="panel-title" >  <span aria-hidden="true" class="glyphicon glyphicon-info-sign"></span>Foil History information</h3>
+                            </div>
+                 <div class="panel-body">
+                     
+                 
+                 
             
-            <?php if(!isset($_POST['numOfFoils'])){ ?>
+            <?php 
+            // Access the page 1st time need to define number of foils having History info to be inserted
+            if(!isset($_POST['numOfFoils'])){ ?>
             <form method="POST" action="new_gem_multi.php">
                 <div class="form-group">
                     <label for="exampleInputFile">How many FOILs do you want to load history information for ?? </label>
@@ -22,15 +32,20 @@ include "head.php";
             </form>
             <?php } ?>
             
-            <?php if(isset($_POST['numOfFoils'])){ 
+            <?php 
+            //  number of foils having History info to be inserted, defined , generate form 
+            if(isset($_POST['numOfFoils'])){ 
                 $num = $_POST['numOfFoils'];
                 ?>
             
             <form method="POST" action="new_gem_multi.php">
-             <input hidden="" value="<?= $num; ?>" name="numOfFoils">
+             <input hidden="" value="<?= $num; ?>" name="foilsnumbersubmitted">
+             <div class="form-group">
+                 
+             </div>
              <div class="form-group">
                 
-                <label for="exampleInputFile"> Pick a FOIL </label>
+                <label for="exampleInputFile"> Pick FOIL/s </label>
                 <div class="row">
                     <div class="col-md-4">
                         <?php for($i = 1 ; $i <= $num; $i++) {?>
@@ -62,6 +77,12 @@ include "head.php";
             </form>
             
            <?php } ?>
+                 </div>
+                 </div>
+             <?php 
+            //  Form Submitted , need to generate XML 
+            if(isset($_POST['foilsnumbersubmitted'])){
+            }?>
 
         </div>
     </div>
@@ -77,7 +98,7 @@ include "foot.php";
 $( "select[class^='chosen-select-foil-']" ).chosen();
 
 $("select[class^='chosen-select-foil-']").on('change', function (evt, params) {
-           $(this).prev().val($(this).chosen().val());      
+           $(this).prev().prev().val($(this).chosen().val());      
 });
 
 </script>
