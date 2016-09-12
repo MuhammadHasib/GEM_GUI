7f7dbfcac65986257dfe8489b184908004049664
ola <ola@olamac.dyndns.cern.ch>
2016-09-12 20:40:53 +0200

    Multi FOILs(s) generate XML update

--- functions/generate_xml.php
+++ functions/generate_xml.php
@@ -10,8 +10,8 @@ include 'globals.php';
 function generateDatasetXml($data) {
 
         /* Error Reporting */
-//    error_reporting(E_ALL);
-//    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
     
     $xml = new DOMDocument("1.0");
     
