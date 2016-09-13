b8686f491e31cfacb5f45f51f45084340b2da47d
ola <ola@olamac.dyndns.cern.ch>
2016-09-13 15:20:25 +0200

    Add Feature: Tracking components generate xml + Edit feature: Foil history not batch field

--- foot.php
+++ foot.php
@@ -25,6 +25,8 @@
 <script type="text/javascript" src="bootstrap-datetimepicker-master/js/locales/bootstrap-datetimepicker.fr.js" charset="UTF-8"></script>
 <script type="text/javascript" src="plugins/chosen_v1.4.2/chosen.proto.js" charset="UTF-8"></script>
 <script type="text/javascript" src="plugins/chosen_v1.4.2/chosen.jquery.js" charset="UTF-8"></script>
+<!-- ace scripts -->
+<script src="ace-master/assets/js/ace-elements.min.js"></script>
 
 <script type="text/javascript">
     function doSomething() {
