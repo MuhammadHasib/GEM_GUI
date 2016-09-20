f0af7d6083d8f8da2542f779af83792304745312
ola <ola@olamac.dyndns.cern.ch>
2016-09-20 15:56:04 +0200

    Add Feature: Tracking components generate xml+ generic globals

--- functions/globals.php
+++ functions/globals.php
@@ -14,13 +14,13 @@ $servername = "int2r1-v.cern.ch:10121/int2r.cern.ch";
  * Useage: set globals with Kind of parts IDs as it will change when db change to Production 
  * Autor: Ola Aboamer [o.aboamer@cern.ch]
  */
-function get_kinds_set_globals($accountname,$password,$servername) {
+function get_kinds_set_globals($accname,$pass,$servname) {
     //load oci8 library if not load automatically
     if (!extension_loaded('oci8')) {
         dl("php_oci8.dll");
     }
     //connect to database     
-    $conn = oci_connect($accountname, $password, $servername);
+    $conn = oci_connect($accname, $pass, $servname);
     if (!$conn) {
         $e = oci_error();
         trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
