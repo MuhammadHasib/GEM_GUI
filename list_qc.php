2e165c5426ee0bde4f59535747748c95d612bf09
ola <ola@olamac.dyndns.cern.ch>
2016-09-28 00:20:36 +0200

    Add feature: Cosmic stand Layout page Add feature: super chamber Add feature: search channels get PIN Include once globals.php ( No triple execution )

--- list_qc.php
+++ list_qc.php
@@ -36,24 +36,17 @@ include "head.php";
                         <span class="label label-warning"><span class="glyphicon glyphicon-asterisk" aria-hidden="true"></span> Notification !!</span>
                           <div class="alert alert-warning" role="alert" style="text-align: center;"><span aria-hidden="true" class="glyphicon glyphicon-wrench"></span> Still Under Development, We'll keep you informed Soon <span aria-hidden="true" class="glyphicon glyphicon-hourglass"></span></div>
                 </li>-->
+
                 <li class="list-group-item">
-                <div class="row">
-                    <div class="col-lg-4 col-md-4 col-sm-8 col-xs-16">
-                        <img  src="images/cosmicStand.png">
-                    </div>
-                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-36">
-                        <div class="panel panel-default">
+                    <div class="panel panel-default">
                         <div class="panel-heading">
                             <h3 class="panel-title">Cosmic Stand layout</h3>
                         </div>
                         <div class="panel-body">
-                            
+                            <img width="10%" src="images/cosmicStand.png">
                             <a href="qc_cosmic_stand_layout.php"><button type="button" class="btn btn-success"> <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Setup</button></a>
                         </div>
                     </div>
-                    </div>
-                    
-                </div>
                 </li>
 
             </ul>
