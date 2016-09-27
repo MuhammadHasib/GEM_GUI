eefdf2cd02c8d866f496885869e08f6db1ebaffc
ola <ola@olamac.dyndns.cern.ch>
2016-09-28 00:17:09 +0200

    Add feature: Cosmic stand Layout page Add feature: super chamber Add feature: search channels get PIN Include once globals.php ( No triple execution )

--- list_qc.php
+++ list_qc.php
@@ -7,13 +7,17 @@ include "head.php";
 <div class="container-fluid">
     <div class="row">
 <?php include "side.php"; ?>
+<!--        <div class="col-xs-6 col-sm-6 col-sm-offset-3 col-md-10 col-md-offset-2 main">-->
         <div class="col-xs-6 col-sm-6 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+            
             <h1 class="page-header">Quality control list</h1>
 
 
-            <ul class="list-group">
+            
 
-                <!--<li class="list-group-item"><div class="panel panel-default">
+                <!--
+                <ul class="list-group">
+                <li class="list-group-item"><div class="panel panel-default">
                 <div class="panel-heading">
                   <h3 class="panel-title">Visual Inspection</h3>
                 </div>
@@ -33,20 +37,29 @@ include "head.php";
                 <!--<li class="list-group-item" style="text-align:center">
                         <span class="label label-warning"><span class="glyphicon glyphicon-asterisk" aria-hidden="true"></span> Notification !!</span>
                           <div class="alert alert-warning" role="alert" style="text-align: center;"><span aria-hidden="true" class="glyphicon glyphicon-wrench"></span> Still Under Development, We'll keep you informed Soon <span aria-hidden="true" class="glyphicon glyphicon-hourglass"></span></div>
-                </li>-->
-
-                <li class="list-group-item">
-                    <div class="panel panel-default">
+                </li>
+                </ul>
+                -->
+                <div class="row">
+                    <div class="col-lg-4 col-md-4 col-sm-8 col-xs-16">
+                        <img  src="images/cosmicStand.png">
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-36">
+                        <div class="panel panel-default">
                         <div class="panel-heading">
                             <h3 class="panel-title">Cosmic Stand layout</h3>
                         </div>
                         <div class="panel-body">
+                            
                             <a href="qc_cosmic_stand_layout.php"><button type="button" class="btn btn-success"> <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Setup</button></a>
                         </div>
                     </div>
-                </li>
+                    </div>
+                    
+                </div>
+                
 
-            </ul>
+            
 
         </div>
 
